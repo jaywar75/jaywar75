e08965051b66bc03e02baa2ebacd4fd5c78171b5
jaywar75 <94010953+jaywar75@users.noreply.github.com>
2022-07-05 23:16:35 -0400

    Update toppings.py

--- toppings.py
+++ toppings.py
@@ -1,3 +1,5 @@
-requested_topping = 'pepperoni'
+requested_topping = 'anchovies'
 if requested_topping != 'anchovies':
     print("Hold the anchovies!")
+else:
+    print('Please enjoy your pizza!')
