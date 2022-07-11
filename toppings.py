dcee06f0e2b8f53beb42e12bd602b4cbf2a8eaaa
jaywar75 <94010953+jaywar75@users.noreply.github.com>
2022-07-10 23:18:57 -0400

    7/10 push and commit from pycharm

--- toppings.py
+++ toppings.py
@@ -1,5 +1,10 @@
-requested_topping = 'anchovies'
-if requested_topping != 'anchovies':
-    print("Hold the anchovies!")
-else:
-    print('Please enjoy your pizza!')
+requested_topping = ['mushrooms', 'extra cheese', 'pepperoni']
+
+if 'mushrooms' in requested_topping:
+    print("Adding Mushrooms")
+if 'pepperoni' in requested_topping:
+    print("Adding Pepperoni")
+if 'extra cheese' in requested_topping:
+    print("Adding Extra Cheese")
+
+print("\nFinished making your pizza, please enjoy!")
