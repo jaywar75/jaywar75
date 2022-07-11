50e6d8be3f5a848d4e275d3fe33714129937ff19
jaywar75 <94010953+jaywar75@users.noreply.github.com>
2022-07-11 18:19:42 -0400

    7/10 push and commit from pycharm

---
+++ tax_engine.py
@@ -0,0 +1,8 @@
+class State:
+    def __init__(self, jurisdiction, category, rate):
+
+        """Initialize attributes of a state tax rate."""
+
+        self.jurisdiction = jurisdiction
+        self.category = category
+        self.rate = rate
